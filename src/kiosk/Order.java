24ab3b6078931598369ab2f569f2325656bb850f
Jo Woojin <dnwls3525@gmail.com>
2023-06-01 19:05:07 +0900

    옵션 개발중

--- src/kiosk/Order.java
+++ src/kiosk/Order.java
@@ -11,13 +11,42 @@ public class Order {
 
     public void AddOrder(Product product) {
         Scanner sc = new Scanner(System.in);
+        int input;
         product.Show();
+        if (product.getSizeupPrice() != 0) { //사이즈업이 있을때
+            Product sizeupProduct = new Product(product.getName(), product.getDescription(), product.getSizeupPrice(), product.getCategory());
+            System.out.println("위 메뉴의 어떤 옵션으로 추가하시겠습니까?");
+            System.out.println("1. Single(W " + product.getPrice() + ")          2. Double(W " + sizeupProduct.getSizeupPrice() + ")");
+            input = sc.nextInt();
+            if (input == 1) { //기본 사이즈
+                product.Show();
+                System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                System.out.println("1.확인          2.취소");
+                input = sc.nextInt();
+                if (input == 1) {
+                    System.out.println(product.getName() + "가 장바구니에 추가되었습니다.");
+                    shoppingBag.add(product);
+                } else if (input == 2) {
+                    System.out.println("장바구니에 추가하지 않았습니다.");
+                }
+            } else if (input == 2) {//사이즈업
+                sizeupProduct.Show();
+                System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                System.out.println("1.확인          2.취소");
+                input = sc.nextInt();
+                if (input == 1) {
+                    System.out.println(sizeupProduct.getName() + "가 장바구니에 추가되었습니다.");
+                    shoppingBag.add(sizeupProduct);
+                } else if (input == 2) {
+                    System.out.println("장바구니에 추가하지 않았습니다.");
+                }
+            }
+        }
+    }
+
+    public void Question(){
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.println("1.확인          2.취소");
-        if (sc.nextInt() == 1) {
-            System.out.println(product.getName() + "가 장바구니에 추가되었습니다.");
-            shoppingBag.add(product);
-        }
     }
     public double getShoppingBag(){//쇼핑백 내용 보여주기, 가격 return
         double totalprice = 0;
--- src/kiosk/Product.java
+++ src/kiosk/Product.java
@@ -7,14 +7,27 @@ public class Product extends Menu {
     //상품 클래스는 이름, 가격, 설명 필드를 가지는 클래스로 만들어주세요.
     //상품 클래스의 이름, 설명 필드는 메뉴 클래스를 상속받아 사용하는 구조로 개발해주세요.
     private double price;
+    private double sizeupPrice;
     private String category;
+
     public Product(String name, String description, double price, String category) {
         super(name, description);
         this.price = price;
         this.category = category;
+        this.sizeupPrice = 0;
+    }
+    public Product(String name, String description, double price, double sizeupPrice, String category)
+    {
+        super(name, description);
+        this.price = price;
+        this.sizeupPrice = sizeupPrice;
+        this.category = category;
     }
     public double getPrice() {
         return price;
+    }
+    public double getSizeupPrice(){
+        return sizeupPrice;
     }
     public String getCategory(){
         return category;
