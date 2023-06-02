68508ff95238a4fda2cf4add83f4be3c35bae26c
Jo Woojin <dnwls3525@gmail.com>
2023-06-02 10:15:48 +0900

    추가기능 완료

--- src/kiosk/Order.java
+++ src/kiosk/Order.java
@@ -1,6 +1,7 @@
 package kiosk;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -14,8 +15,8 @@ public class Order {
         Scanner sc = new Scanner(System.in);
         int input;
 
-        if (product.getSizeupPrice() != 0) { //사이즈업이 있을때
-            product.Show();
+        if (product.getSizeupPrice() != 0) { //사이즈업 옵션이 있을때
+            product.Show();// 메뉴 | W 가격 | 이름
             Product sizeupProduct = new Product(product.getName(), product.getDescription(), product.getSizeupPrice(), product.getCategory());
             System.out.println("위 메뉴의 어떤 옵션으로 추가하시겠습니까?");
             System.out.println("1. Single(W " + product.getPrice() + ")          2. Double(W " + sizeupProduct.getPrice() + ")");
@@ -44,15 +45,18 @@ public class Order {
     }
     public double getShoppingBag(){//쇼핑백 내용 보여주기, 가격 return
         double totalprice = 0;
-        for (Product item : shoppingBag) {
-            item.Show();
-            totalprice += item.getPrice(); //가격계산
+        int EA;
+        HashSet<Product> productCountSet = new HashSet<Product>(shoppingBag);
+        for (Product item : productCountSet) {
+            EA = Collections.frequency(shoppingBag,item);
+            item.Show(EA);
+            totalprice += item.getPrice() * EA; //가격계산
         }
         totalprice=Math.round((totalprice*100))/100.0;//소수점 둘째자리까지 나타내고 반올림 ( 자바는 IEEE 754 부동 소수점 방식 - 근사치 제공에 의해 소수 오차 발생 방지)
         return totalprice;
     }
     public int CompleteOrder() {
-        soldProduct.addAll(shoppingBag);
+        soldProduct.addAll(shoppingBag); //총 판매상품 hashset에 쇼핑백 목록 넣기
         shoppingBag.clear();//
         counter++;          //대기번호 +1
         return counter;
--- src/kiosk/Product.java
+++ src/kiosk/Product.java
@@ -1,5 +1,7 @@
 package kiosk;
 
+import java.util.Objects;
+
 public class Product extends Menu {
 
     //필수요구사항
@@ -40,5 +42,28 @@ public class Product extends Menu {
     {
         System.out.printf("%-20s | W %s |%s\n", getName(), price,getDescription());
     }
+    public void Show(int EA)
+    {
+        System.out.printf("%-20s | W %s | %s개 |%s\n", getName(), price,EA,getDescription());
+    }
 
+
+    //equals, hashcode 재정의
+    //HashSet이 새롭게 선언하고 추가한 사이즈업메뉴를 동일객체로 인식하지 못함.
+    //이유 :기존노드와 같은지 확인하려는 작업으로 추가할 노드의 equals()와 hashCode()를 호출하게 되는데, 이 부분에서 같은 객체로 인식하지 못함
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Product) {
+            Product temp = (Product)obj;
+            return this.getName().equals(temp.getName()) &&
+                    this.getPrice() == temp.getPrice() &&
+                    this.getSizeupPrice() == temp.getSizeupPrice() &&
+                    this.getDescription().equals(temp.getDescription()) &&
+                    this.getCategory().equals(temp.getCategory());
+        }
+        return false;
+    }
+    public int hashCode(){
+        return Objects.hash(getName(),getDescription(),getPrice(),getSizeupPrice(),getCategory());
+    }
 }
